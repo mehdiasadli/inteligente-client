4eefc25d7f1c4e0e921d188389233f1c6f8b19e7
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2023-09-26 18:23:09 +0400

    add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+import { inject } from '@vercel/analytics';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
